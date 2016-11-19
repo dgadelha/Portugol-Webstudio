98d473e21ad2b0346f49d666e1f0b4aa86d93ba0
Douglas G <douglas@gadeco.com.br>
2016-11-19 09:28:22 -0300

    Virando um IDE de verdade

--- Aplicativo/Modelos/IDE.php
+++ Aplicativo/Modelos/IDE.php
@@ -6,84 +6,46 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Portugol Webstudio</title>
 
-	<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/ax5ui/ax5ui-layout/master/dist/ax5layout.css" />
-	<style type="text/css" media="screen">
-		body{
-			background-color: #333;
-		}
-		#editor, #output, #input {
-			margin: 0;
-			width: 100%;
-			height: 100%;
-			font-size: 14px;
-		}
-
-		#layout {
-			position: absolute;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			right: 0;
-			width: 100%;
-			height: 100%;
-		}
-
-		#submit {
-			display: block;
-			margin: 0;
-			width: 100%;
-			font-size: 22px;
-			border: none;
-			background-color: #333;
-			color: #FFF;
-			height: 40px;
-			border-radius: 0;
-		}
-
-		#submit:hover {
-			background-color: #222;
-		}
-
-		#submit[disabled], #submit[disabled]:hover {
-			border: 1px solid #333;
-			background-color: #444;
-		}
-
-		.title {
-			margin: 0;
-			padding: 5px;
-			background-color: #444;
-			border: 1px solid #333;
-			color: #FFF;
-			text-align: center;
-			text-transform: uppercase;
-		}
-		[data-ax5layout]>[data-splitter]{
-			background-color: #666;
-		}
-	</style>
+	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/ax5ui/ax5ui-layout/master/dist/ax5layout.css">
+	<link rel="stylesheet" type="text/css" href="<?=$siteURL;?>assets/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=$siteURL;?>assets/ide.css">
 </head>
 <body>
-	<div data-ax5layout="ax1" data-config="{layout:'dock-panel'}" id="layout">
-		<!-- <div style="padding-top:10px; margin-botton:2px; width:100%; text-align:center; background-color:#333;" data-dock-panel="{dock:'top',split:true,height:140,minHeight:140,maxHeight:140}">
-			<img style="height:128px;" src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/lite.png"/>
-		</div> -->
-		<div style="text-align:center; padding-top:6px; width:100%; background-color:#333;" data-dock-panel="{dock:'left',split:true,height:250,minHeight:250,maxHeight:250,width:64,minWidth:64,maxWidth:64}">
-			<img style="height:40px;" src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/lite.png"/>
-			<button style="margin-top: 5px;" id="submit"><img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/resultset_next.png"/></button>
-		</div>
+	<div id="ide">
+		<ul class="tabs">
+			<li><a id="anchor-inicio" href="#tab-inicio">Portugol Webstudio <span class="portugol-icon"></span><span class="spacing-icon"></span><span class="add-icon" onClick="addTab();return false"></span></a></li>
+		</ul>
 
-		<div data-dock-panel="{dock:'bottom',split:true,height:150,minHeight:10,maxHeight:300}">
-			<pre id="output"></pre>
-		</div>
-
-		<div data-dock-panel="{dock:'right',split:true,width:250,minWidth:100,maxWidth:350}">
-			<p class="title">Entrada de Dados</p>
-			<pre id="input"></pre>
-		</div>
-
-		<div data-dock-panel="{dock:'center'}">
-			<pre id="editor"></pre>
+		<div class="tab" id="tab-inicio">
+			<div class="tab-content">
+				<div class="row" id="btl">
+					<div class="col-md-3">
+						<button onClick="addTab()">
+							<img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/programar.png">
+							<span>Programar</span>
+						</button>
+					</div>
+					<div class="col-md-3">
+						<button>
+							<img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/ajuda.png">
+							<span>Ajuda</span>
+						</button>
+					</div>
+					<div class="col-md-3">
+						<button>
+							<img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/videoaulas.png">
+							<span>Videoaulas</span>
+						</button>
+					</div>
+					<div class="col-md-3">
+						<button>
+							<img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/lite/bibliotecas.png">
+							<span>Bibliotecas</span>
+						</button>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
@@ -91,8 +53,8 @@
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.5/ace.js"></script>
 	<script type="text/javascript" src="https://cdn.rawgit.com/ax5ui/ax5core/master/dist/ax5core.min.js?5cbc9cc42d9824913945da0260f2eb9d5773b317"></script>
 	<script type="text/javascript" src="https://cdn.rawgit.com/ax5ui/ax5ui-layout/master/dist/ax5layout.min.js?c28a8add0b87fc4d5ad0ce4b1bb7ff6e7c681ea1"></script>
-	<script type="text/javascript" src="<?=$siteURL;?>mode-portugol.js"></script>
-	<script type="text/javascript">var d={ajaxUrl:"<?=$ajaxURL;?>",editor:null,output:null,input:null,running:false,scrollDown:function(){var r=d.output.session.getLength()-1;var c=d.output.session.getLine(r).length;d.output.gotoLine(r+1,c)}};</script>
-	<script type="text/javascript" src="<?=$siteURL;?>ide.js"></script>
+	<script type="text/javascript" src="<?=$siteURL;?>assets/mode-portugol.js"></script>
+	<script type="text/javascript">var d={ajaxUrl:"<?=$ajaxURL;?>",tabs:[],scrollDown:function(tid){var r=d.tabs[tid].output.session.getLength()-1;var c=d.tabs[tid].output.session.getLine(r).length;d.tabs[tid].output.gotoLine(r+1,c)}};</script>
+	<script type="text/javascript" src="<?=$siteURL;?>assets/ide.js"></script>
 </body>
 </html>
---
+++ assets/ide.js
@@ -0,0 +1,202 @@
+var lut = []; for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+function e7() { /*! @see: http://stackoverflow.com/a/21963136 !*/
+	var d0 = Math.random()*0xffffffff|0;
+	var d1 = Math.random()*0xffffffff|0;
+	var d2 = Math.random()*0xffffffff|0;
+	var d3 = Math.random()*0xffffffff|0;
+	return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+	lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+	lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+	lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+}
+
+function addTab(name = "Sem título", content = "") {
+	var id = "t" + e7();
+	$(".tabs").append("<li><a href=\"#tab-" + id + "\" id=\"anchor-" + id + "\"><span class=\"portugol-icon\"></span> " + name + " <span class=\"close-icon\"></span></a></li>");
+
+	var tpl = '<div id="tab-' + id + '" class="tab">';
+	tpl += '<div data-ax5layout="' + id + '-ax" data-config="{layout:\'dock-panel\'}" class=\"ide-layout\" id="' + id + '-layout">';
+	tpl += '<div class="tbar" data-dock-panel="{dock:\'left\',split:false,height:250,minHeight:250,maxHeight:250,width:64,minWidth:64,maxWidth:64}">';
+	tpl += '<button style="margin-top: 5px;" class="submit"><img src="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/grande/resultset_next.png"></button>';
+	tpl += '</div>';
+
+	tpl += '<div data-dock-panel="{dock:\'bottom\',split:true,height:150,minHeight:50,maxHeight:300}">';
+	tpl += '<pre id="' + id + '-output" class="output"></pre>';
+	tpl += '</div>';
+
+	tpl += '<div data-dock-panel="{dock:\'right\',split:true,width:250,minWidth:100,maxWidth:350}">';
+	tpl += '<p class="title">Entrada de Dados</p>';
+	tpl += '<pre id="' + id + '-input" class="input"></pre>';
+	tpl += '</div>';
+
+	tpl += '<div data-dock-panel="{dock:\'center\'}">';
+	tpl += '<pre id="' + id + '-editor" class="editor"></pre>';
+	tpl += '</div>';
+	tpl += '</div>';
+	tpl += '</div>';
+
+	$("#ide").append(tpl);
+	bindHTML();
+
+	d.tabs[id] = {
+		editor: null,
+		input: null,
+		output: null,
+		running: false
+	};
+
+	d.tabs[id].editor = ace.edit(id + "-editor");
+	d.tabs[id].editor.$blockScrolling = Infinity;
+	d.tabs[id].editor.setTheme("ace/theme/tomorrow_night_eighties");
+	d.tabs[id].editor.setFontSize(14);
+	d.tabs[id].editor.getSession().setMode("ace/mode/portugol");
+	d.tabs[id].editor.getSession().setValue("programa {\n\tfuncao inicio() {\n\t\t\n\t}\n}");
+	d.tabs[id].editor.setShowPrintMargin(false);
+	d.tabs[id].editor.getSession().setUseSoftTabs(true);
+	d.tabs[id].editor.gotoLine(3);
+	d.tabs[id].editor.selection.moveTo(2, 2);
+
+	d.tabs[id].input = ace.edit(id + "-input");
+	d.tabs[id].input.$blockScrolling = Infinity;
+	d.tabs[id].input.setTheme("ace/theme/tomorrow_night_eighties");
+	d.tabs[id].input.setFontSize(14);
+	d.tabs[id].input.getSession().setMode("ace/mode/sh");
+	d.tabs[id].input.getSession().setUseSoftTabs(true);
+	d.tabs[id].input.setShowPrintMargin(false);
+	d.tabs[id].input.renderer.setShowGutter(false);
+
+	d.tabs[id].output = ace.edit(id + "-output");
+	d.tabs[id].output.$blockScrolling = Infinity;
+	d.tabs[id].output.setTheme("ace/theme/tomorrow_night_eighties");
+	d.tabs[id].output.setFontSize(14);
+	d.tabs[id].output.getSession().setMode("ace/mode/sh");
+	d.tabs[id].output.getSession().setValue("");
+	d.tabs[id].output.setShowPrintMargin(false);
+	d.tabs[id].output.renderer.setShowGutter(false);
+	d.tabs[id].output.setReadOnly(true);
+	d.tabs[id].output.selection.moveTo(0, 2);
+
+	d.tabs[id].editor.focus();
+	$("#anchor-" + id).trigger("click");
+
+	$("#anchor-" + id + " .close-icon").bind("click", function() {
+		var id = $(this).parent().attr("id").substr(7);
+
+		d.tabs[id].editor.destroy();
+		d.tabs[id].input.destroy();
+		d.tabs[id].output.destroy();
+
+		d.tabs[id] = false;
+		delete d.tabs[id];
+
+		$(this).parent().parent().remove();
+		$("#tab-" + id).remove();
+		$("#anchor-inicio").trigger("click");
+	});
+
+	$("#tab-" + id + " .submit").bind("click", function() {
+		var id = $(this).parent().parent().attr("id");
+		id = id.substr(0, id.length - 7);
+		if (d.tabs[id].running) return;
+
+		d.tabs[id].running = true;
+		d.tabs[id].editor.setReadOnly(true);
+		d.tabs[id].input.setReadOnly(true);
+		$("#tab-" + id + " .submit").attr("disabled", "disabled");
+		d.tabs[id].output.getSession().setValue("");
+		d.scrollDown(id);
+
+		$.ajax({
+			_id: id,
+			async: true,
+			cache: false,
+			crossDomain: true,
+			data: { codigo: d.tabs[id].editor.getSession().getValue(), entrada: d.tabs[id].input.getSession().getValue() },
+			dataType: 'json',
+			method: 'POST',
+			processData: true,
+			timeout: 60000,
+			url: d.ajaxUrl,
+
+			error: function(jqXHR, textStatus, errorThrown) {
+				var id = this._id;
+				d.tabs[id].output.getSession().setValue(d.output.getSession().getValue() + "Falha ao enviar o código para o servidor:\n" + textStatus + " " + errorThrown + "\n\n$ ");
+				d.tabs[id].running = false;
+				d.tabs[id].editor.setReadOnly(false);
+				d.tabs[id].input.setReadOnly(false);
+				$("#tab-" + id + " .submit").removeAttr("disabled");
+				d.scrollDown(id);
+			},
+
+			success: function(data, textStatus, jqXHR) {
+				var id = this._id;
+				d.tabs[id].output.getSession().setValue(d.tabs[id].output.getSession().getValue() + data.output);
+				d.tabs[id].running = false;
+				d.tabs[id].editor.setReadOnly(false);
+				d.tabs[id].input.setReadOnly(false);
+				$("#tab-" + id + " .submit").removeAttr("disabled");
+				d.scrollDown(id);
+			}
+		});
+	});
+}
+
+function bindHTML() {
+	$("ul.tabs").each(function(){
+		var $active, $content, $links = $(this).find("a");
+
+		$active = $($links.filter("[href=\"" + location.hash + "\"]")[0] || $links[0]);
+		$active.addClass("active");
+
+		$content = $($active[0].hash);
+
+		$links.not($active).each(function () {
+			$(this.hash).hide();
+		});
+
+		$(this).on("click", "a", function(e){
+			$active.removeClass("active");
+			$content.hide();
+
+			$active = $(this);
+			$content = $(this.hash);
+
+			$active.addClass("active");
+			$content.show();
+
+			var ttid = $content.attr("id");
+
+			if (ttid.startsWith("tab-t")) {
+				var id = ttid.substr(4);
+
+				d.tabs[id].editor.resize();
+				d.tabs[id].output.resize();
+				d.tabs[id].input.resize();
+
+				if (!d.tabs[id].ax5initialized) {
+					d.tabs[id].ax5initialized = true;
+
+					$("#" + id + "-layout").ax5layout({
+						onResize: function() {
+							var id = this.name.substr(0, this.name.length - 3)
+
+							if (id.startsWith("t")) {
+								if (!d.tabs[id]) return;
+
+								d.tabs[id].editor.resize();
+								d.tabs[id].output.resize();
+								d.tabs[id].input.resize();
+							}
+						}
+					});
+				}
+			}
+
+			e.preventDefault();
+		});
+	});
+}
+
+$(window).bind("load", function() {
+	bindHTML();
+});
