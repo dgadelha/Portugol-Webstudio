9cfb1bee3fe1fea2af738d3397abfebd22a42502
Douglas G <douglas@gadeco.com.br>
2016-11-19 11:02:01 -0300

    Adicionar favicon

--- Aplicativo/Modelos/IDE.php
+++ Aplicativo/Modelos/IDE.php
@@ -6,6 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Portugol Webstudio</title>
 
+	<link rel="shortcut icon" href="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/pequeno/light_pix.png">
+	<link rel="icon" href="https://raw.githubusercontent.com/UNIVALI-LITE/Portugol-Studio/master/src/br/univali/ps/ui/icones/pequeno/light_pix.png">
+
 	<link href="https://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/ax5ui/ax5ui-layout/master/dist/ax5layout.css?7544f5a6f789ebaf83e5f070a19a881725c16b6f">
 	<link rel="stylesheet" type="text/css" href="<?=$siteURL;?>assets/bootstrap.min.css">
