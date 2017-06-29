27b0d1c79b1d354705f6d4d96817540d73af6bec
Guilherme Scaranse <guiscaranse@gmail.com>
2017-06-28 22:27:20 -0300

    Express

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
