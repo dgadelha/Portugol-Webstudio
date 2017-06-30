7701cc3eb2b48b522b394783eb0dd6fc141fab46
Guilherme Scaranse <guiscaranse@gmail.com>
2017-06-30 17:37:13 -0300

    Corrigido bug do pty.js que transformava dois outputs em um só
    Aparentemente todo trigger com menos de 0.1s ou interpretado como instantâneo causaria o pty.js achar que é a mesma entrada apenas com delay unido-as como uma só string. Isto ocasionaria um bug na hora de detectar as palavras reservadas pois seria necessário extrair elas da string

--- libs/runtime.py
+++ libs/runtime.py
@@ -1,13 +1,28 @@
-import os, pexpect, sys
+import os, pexpect, sys, time
+def filter(path):
+    reserved = ["~|^!+RUNTIME+!^|~", "~|^!+START+!^|~", "~|^!+END+!^|~", "~|^!+INPUT+!^|~"]
+    banned_libs = ["Arquivos", "Graficos", "Mouse", "Sons", "Teclado"]
+    with open(path, 'r') as content_file: content = str(content_file.read()).rsplit()
+    if any(rsr in x for rsr in reserved for x in content):
+        raise Exception("Existem palavras reservadas do webstudio no seu código! Palavras reservadas se assemelham à ~|^!+PALAVRA+!^|~")
+    elif any(rsr in x for rsr in banned_libs for x in content):
+        raise Exception("Existem bibliotecas não permitidas do webstudio no seu código! Se deseja utilizar todas as bibliotecas instale o Portugol Studio.")
 while(True):
-    print("Olá mundo!")
     command = str(input())
     if("~|^!+RUNTIME+!^|~" in command):
+        time.sleep(0.1) # Sleep para pty.js não juntar as linhas
         print("~|^!+START+!^|~")
         path = os.getcwd() + "/" + command.replace("~|^!+RUNTIME+!^|~", "")
-        comando = "java -Dfile.encoding=UTF-8 -Xms128m -Xmx512m -d64 -jar \"" + os.getcwd() + "/libs/portugol-console.jar" +"\" \""+ path +"\""
-        child = pexpect.spawnu(comando)
-        child.interact()
+        try:
+            time.sleep(0.1) # Sleep para pty.js não juntar as linhas
+            filter(path)
+        except Exception as e:
+            print("ERRO!", e)
+        else:
+            comando = "java -Dfile.encoding=UTF-8 -Xms128m -Xmx512m -d64 -jar \"" + os.getcwd() + "/libs/portugol-console.jar" +"\" \""+ path +"\""
+            child = pexpect.spawnu(comando)
+            child.interact()
+        time.sleep(0.1)
         print("~|^!+END+!^|~")
         if(os.path.isfile(path)):
             os.unlink(path)
