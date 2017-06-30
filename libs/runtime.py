d368e03fba21b640ef86cc1d232bafc60e973cfd
Guilherme Scaranse <guiscaranse@gmail.com>
2017-06-30 16:12:44 -0300

    Removidos \n desnecessários

--- libs/runtime.py
+++ libs/runtime.py
@@ -3,11 +3,11 @@ while(True):
     print("Olá mundo!")
     command = str(input())
     if("~|^!+RUNTIME+!^|~" in command):
-        print("~|^!+START+!^|~\n")
-        path = os.getcwd() + "/" +command.replace("~|^!+RUNTIME+!^|~", "")
+        print("~|^!+START+!^|~")
+        path = os.getcwd() + "/" + command.replace("~|^!+RUNTIME+!^|~", "")
         comando = "java -Dfile.encoding=UTF-8 -Xms128m -Xmx512m -d64 -jar \"" + os.getcwd() + "/libs/portugol-console.jar" +"\" \""+ path +"\""
         child = pexpect.spawnu(comando)
         child.interact()
-        print("\n~|^!+END+!^|~")
+        print("~|^!+END+!^|~")
         if(os.path.isfile(path)):
             os.unlink(path)
