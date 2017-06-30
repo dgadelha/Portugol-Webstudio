80fa50b8f21155049effc29072cd0f9d28f9953e
Guilherme Scaranse <guiscaranse@gmail.com>
2017-06-29 22:02:33 -0300

    Agora rota / renderiza index corretamente

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index');
 });
 
 module.exports = router;
