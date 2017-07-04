69e9e34d97356025f70f326d1832cdb4a7a4780d
Douglas Gadêlha <dgadelha@users.noreply.github.com>
2017-07-03 21:15:46 -0300

    Alterar o script de deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-remote_path=/home/dgadelha/Portugol-Webstudio
-echo "Preparando para fazer deploy em $remote_path..."
-ssh ${STUDIOHOST} "cd $remote_path && pm2 restart 0"
-cd $remote_path
-git pull
-exit
+ssh ${STUDIOHOST} "pm2 restart 0"
