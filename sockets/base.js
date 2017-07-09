1c2b37ca5652f455fec17e0f94a7fa4247a562db
Guilherme Scaranse <guiscaranse@gmail.com>
2017-07-09 14:08:12 -0300

    Adicionado comentários e indicador de início e fim

--- sockets/base.js
+++ sockets/base.js
@@ -1,7 +1,10 @@
-module.exports = function (io){
+module.exports = function(io) {
   var fs = require('fs');
   var pty = require('pty.js');
-  io.on('connection', function(socket){
+  io.on('connection', function(socket) {
+    /*
+    Usuário se conectou então nós criamos o terminal e definimos a trava (listen)
+    */
     var listen = false;
     var term = pty.spawn('./libs/runtime/linux/runtime', [""], {
       name: 'xterm',
@@ -10,53 +13,77 @@ module.exports = function (io){
       cwd: require('path').dirname(require.main.filename) + "/../",
       env: process.env
     });
-    var file = "libs/runtime/__temp/"+ Math.random().toString(12).substring(7) +".por";
-    // Create terminal
-    socket.on("input", function(data){
-      if(!listen){
-      socket.emit('output', "Iniciando compilação...\n");
-      fs.writeFile(__dirname + "/../" + file, data, function(err){}); // Escrevemos o código em portugol temporariamente
-      console.log("~|^!+RUNTIME+!^|~" + file);
-      term.write("~|^!+RUNTIME+!^|~" + file + "\r");
-      }else{
+    // Arquivo temporário onde o código do portugol será armazenado
+    var file = "libs/runtime/__temp/" + Math.random().toString(12).substring(7) + ".por";
+
+    /*
+    socket.on(input):
+    trigger responsável por receber o código do portugol, escrever no arquivo temporário e passar para o RUNTIME
+    */
+    socket.on("input", function(data) {
+      // Verifica se a trava está ativa e o código está ouvindo inputs do usuário. Se estiver ativa, emite a mensagem indicando para aguardar
+      if (!listen) {
+        var data = new Date(); // Autoexplicativo
+        var hora = (data.getHours() < 10 ? "0" : "") + data.getHours();
+        var minutos = (data.getMinutes() < 10 ? "0" : "") + data.getMinutes();
+        var segundos = (data.getSeconds() < 10 ? "0" : "") + data.getSeconds();
+        socket.emit('output', "Iniciando compilação... (" + hora + ":" + minutos + ":" + segundos + ")\n"); // Envia mensagem dizendo que vai compilar
+        fs.writeFile(__dirname + "/../" + file, data, function(err) {}); // Escrevemos o código em portugol temporariamente
+        term.write("~|^!+RUNTIME+!^|~" + file + "\r"); // Indicamos qual arquivo o RUNTIME deve ler
+      } else {
         socket.emit('output', "\nAguarde o fim da execução!");
       }
     });
-
-    socket.on("response", function(resp){
-      if(listen){
-        resp = resp.replace("~|^!+", "").replace("+!^|~", "");
-        term.write(resp + "\r");
+    /*
+    socket.on(response):
+    trigger resposável por receber quaisquer respostas que o usuário precisar enviar para o código
+    */
+    socket.on("response", function(resp) {
+      if (listen) {
+        resp = resp.replace("~|^!+", "").replace("+!^|~", ""); // Filtro de palavras reservadas
+        term.write(resp + "\r"); // Escreve input para o console virtual
       }
     });
-    // Listen on the terminal for output and send it to the client
-    term.on('data', function(data){
-      data = data.replace("~|^!+INPUT+!^|~", "");
+    /*
+    term.on(data):
+    trigger que é ativado quando existe movimentação de texto no console virtual.
+    É responsável por escrever de volta as informações por console, e determinar a trava de leitura e escrita.
+    */
+    term.on('data', function(data) {
+      data = data.replace("~|^!+INPUT+!^|~", ""); // Filtro de palavra reservada descontinuada
       //console.log(data + " = " + data.indexOf("~|^!+START+!^|~"))
-      if(listen){
-        if(data.includes("~|^!+END+!^|~")){
-          listen = false;
-          socket.emit('hide-response', "");
-        }else{
-          socket.emit('output', data);
+      if (listen) { // Verifica se está executando alguma coisa
+        // Portugol está sendo executado no console
+        if (data.includes("~|^!+END+!^|~")) { // Verifica se RUNTIME indicou que execução acabou
+          // RUNTIME indicou que execução acabou
+          listen = false; // Define que não está executando (ouvindo) nada
+          socket.emit('hide-response', ""); // Desliga input de resposta
+          socket.emit('output', "Execução terminou.\n");
+        } else {
+          // Execução não acabou
+          socket.emit('output', data); // Emite para o cliente o texto do console
         }
-      }else if(data.includes("~|^!+START+!^|~")){
-        listen = true;
-        socket.emit('show-response', "");
+      } else if (data.includes("~|^!+START+!^|~")) { // Não está executando nada, ainda, então verifica se o RUNTIME indica que a execução começou
+        // Execução começou
+        listen = true; // Define que está sendo executado e NodeJS deve ficar no aguardo
+        socket.emit('show-response', ""); // Liga input de resposta
       }
     });
 
 
-    // Listen for a resize request and update the terminal size
-    socket.on('resize', function(data){
+    // Espera por um pedido de redimensionamento e atualiza tamanho do terminal
+    socket.on('resize', function(data) {
       term.resize(data[0], data[1]);
     });
 
-    // When socket disconnects, destroy the terminal
-    socket.on("disconnect", function(){
-      if (fs.existsSync(__dirname + "/../"+ file)) {
-          fs.unlinkSync(__dirname + "/../"+ file);
-          console.log('Found file');
+    /*
+    socket.on(disconnect):
+    Quando usuário desconecta, destroi o console e elimina o arquivo temporário, caso o RUNTIME não tenha deletado ainda
+    */
+    socket.on("disconnect", function() {
+      if (fs.existsSync(__dirname + "/../" + file)) {
+        fs.unlinkSync(__dirname + "/../" + file);
+        console.log('Found file');
       }
       term.destroy();
       console.log("bye");
