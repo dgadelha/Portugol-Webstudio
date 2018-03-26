575443400fab57158d9a10590dd16531286e4280
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-25 22:58:13 -0300

    Teste direto

--- tests/teste_UI.js
+++ tests/teste_UI.js
@@ -16,18 +16,13 @@ module.exports = {
 
     'Usar IDE' : function(client) {
         client
-            .click('li#j1_1 > i')
-            .pause(1000)
-            .waitForElementVisible('a#j1_2_anchor', 5000)
-            .pause(1000)
-            .click('a#j1_2_anchor')
-            .waitForElementVisible('a#exemplo-go', 10000)
-            .click('a#exemplo-go')
-            .pause(5000)
+            .waitForElementVisible('.action-add', 5000)
+            .click('.action-add')
+            .waitForElementVisible('iframe', 5000)
             .frame(0)
             .click('#submit-btn')
             .pause(5000)
-            .assert.containsText('pre#output', 'Olá mundo')
+            .assert.containsText('#output .ace_content', 'Programa finalizado')
             .end();
     }
 }
