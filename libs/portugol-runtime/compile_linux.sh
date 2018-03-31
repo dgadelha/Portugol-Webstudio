5dc147049df0d10a61acc6e86b74299cddba9ecd
Guilherme Scaranse <guiscaranse@gmail.com>
2018-03-31 17:42:26 -0300

    Readicionado .so [skip ci]

--- libs/portugol-runtime/compile_linux.sh
+++ libs/portugol-runtime/compile_linux.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
 dotnet restore
 dotnet publish -r win-x64
 rm -rf ./bin/Debug/netcoreapp2.0/win-x64/publish
 dotnet publish -r debian.8-x64
 rm -rf ./bin/Debug/netcoreapp2.0/debian.8-x64/publish
+rm -rf ./dist/
 mkdir dist
 mv ./bin/Debug/netcoreapp2.0/win-x64/ ./dist/win-x64/
 mv ./bin/Debug/netcoreapp2.0/debian.8-x64/ ./dist/debian.8-x64/
