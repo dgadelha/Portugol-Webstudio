6b84e01a91e5de21c143001f53c2e8205b700aaf
Douglas Gadêlha <dgadelha@live.com>
2018-08-14 13:59:03 -0300

    Tabs -> Spaces e UTF-8-BOM -> UTF-8

--- public/assets/editor/editor.js
+++ public/assets/editor/editor.js
@@ -1,185 +1,185 @@
 var running = false, editor, input = "", output, socket;
 
 function scrollDown() {
-	var r = output.session.getLength() - 1;
-	var c = output.session.getLine(r).length;
-	output.gotoLine(r + 1, c);
+    var r = output.session.getLength() - 1;
+    var c = output.session.getLine(r).length;
+    output.gotoLine(r + 1, c);
 }
 
 $(window).bind("load", function() {
-	$("#splash").slideUp(300, function() {$(this).remove() });
+    $("#splash").slideUp(300, function() {$(this).remove() });
 
-	editor = ace.edit("editor");
-	editor.$blockScrolling = Infinity;
-	editor.setTheme("ace/theme/portugol");
-	editor.setFontSize(14);
-	editor.getSession().setMode("ace/mode/portugol");
-	editor.setShowPrintMargin(false);
-	editor.getSession().setUseSoftTabs(true);
+    editor = ace.edit("editor");
+    editor.$blockScrolling = Infinity;
+    editor.setTheme("ace/theme/portugol");
+    editor.setFontSize(14);
+    editor.getSession().setMode("ace/mode/portugol");
+    editor.setShowPrintMargin(false);
+    editor.getSession().setUseSoftTabs(true);
 
-	var found = false;
-	if (ecid != "") {
-		if (window.parent.portugol.codes[ecid]) {
-			found = true;
-			editor.getSession().setValue(window.parent.portugol.codes[ecid]);
-			delete window.parent.portugol.codes[ecid];
-		}
-	}
+    var found = false;
+    if (ecid != "") {
+        if (window.parent.portugol.codes[ecid]) {
+            found = true;
+            editor.getSession().setValue(window.parent.portugol.codes[ecid]);
+            delete window.parent.portugol.codes[ecid];
+        }
+    }
 
-	if (!found) {
-		editor.getSession().setValue("programa {\n\tfuncao inicio() {\n\t\t\n\t}\n}\n");
-		editor.gotoLine(3);
-		editor.selection.moveTo(2, 2);
-	}
+    if (!found) {
+        editor.getSession().setValue("programa {\n\tfuncao inicio() {\n\t\t\n\t}\n}\n");
+        editor.gotoLine(3);
+        editor.selection.moveTo(2, 2);
+    }
 
-	output = ace.edit("output");
-	output.$blockScrolling = Infinity;
-	output.setTheme("ace/theme/portugol");
-	output.setFontSize(14);
-	output.getSession().setMode("ace/mode/text");
-	output.getSession().setValue("");
-	output.setShowPrintMargin(false);
-	output.renderer.setShowGutter(false);
-	output.setReadOnly(true);
-	output.selection.moveTo(0, 2);
-	output.keyBinding.addKeyboardHandler({
-		handleKeyboard: function(data, hash, keyString, keyCode, event) {
-			console.log("handleKeyboard(data = '" + data + "', hash = '" + hash + "', keyString = '" + keyString + "', keyCode = '" + keyCode + "', event = '" + event + "')");
+    output = ace.edit("output");
+    output.$blockScrolling = Infinity;
+    output.setTheme("ace/theme/portugol");
+    output.setFontSize(14);
+    output.getSession().setMode("ace/mode/text");
+    output.getSession().setValue("");
+    output.setShowPrintMargin(false);
+    output.renderer.setShowGutter(false);
+    output.setReadOnly(true);
+    output.selection.moveTo(0, 2);
+    output.keyBinding.addKeyboardHandler({
+        handleKeyboard: function(data, hash, keyString, keyCode, event) {
+            console.log("handleKeyboard(data = '" + data + "', hash = '" + hash + "', keyString = '" + keyString + "', keyCode = '" + keyCode + "', event = '" + event + "')");
 
-			if (running == false) {
-				console.log("-> Evento de Keyboard IGNORADO - o programa não está em execução!");
-				return;
-			}
+            if (running == false) {
+                console.log("-> Evento de Keyboard IGNORADO - o programa não está em execução!");
+                return;
+            }
 
-			if (keyCode == 13 || keyString == "return") { // enter
-				console.log("-> Evento de Keyboard: ENTER (input: " + input + ")");
+            if (keyCode == 13 || keyString == "return") { // enter
+                console.log("-> Evento de Keyboard: ENTER (input: " + input + ")");
 
-				// Enviar os dados de input pro socket
-				if (input != "") {
-					console.log("Input: " + input);
-					socket.emit("response", input.trim() + "\r");
-					output.getSession().setValue(output.getSession().getValue() + "\n");
-					scrollDown();
-				}
-			} else if (keyCode == 8 || keyString == "backspace") { // backspace
-				console.log("-> Evento de Keyboard: BACKSPACE (input: " + input + ")");
+                // Enviar os dados de input pro socket
+                if (input != "") {
+                    console.log("Input: " + input);
+                    socket.emit("response", input.trim() + "\r");
+                    output.getSession().setValue(output.getSession().getValue() + "\n");
+                    scrollDown();
+                }
+            } else if (keyCode == 8 || keyString == "backspace") { // backspace
+                console.log("-> Evento de Keyboard: BACKSPACE (input: " + input + ")");
 
-				if (input.length >= 1) {
-					input = input.substring(0, input.length - 1);
-					output.getSession().setValue(output.getSession().getValue().substring(0, output.getSession().getValue().length - 1));
-					scrollDown();
-				}
-			} else if (typeof event == 'undefined' && !event) {
-				console.log("-> Evento de KeyBoard: " + keyString);
+                if (input.length >= 1) {
+                    input = input.substring(0, input.length - 1);
+                    output.getSession().setValue(output.getSession().getValue().substring(0, output.getSession().getValue().length - 1));
+                    scrollDown();
+                }
+            } else if (typeof event == 'undefined' && !event) {
+                console.log("-> Evento de KeyBoard: " + keyString);
 
-				if (keyString != "" && keyString != "\n" && keyString != "\r\n" && keyString != "\r") {
-					input += keyString;
-					output.getSession().setValue(output.getSession().getValue() + keyString);
-					scrollDown();
-				} else {
-					// @TODO: keyCode -> HEX
-					// input += "\xHEX";
-					console.log("-> Evento de Keyboard IGNORADO - keyString vazia e suporte a HEX não concluído");
-				}
-			} else {
-				console.log("-> Evento de Keyboard: ", event);
+                if (keyString != "" && keyString != "\n" && keyString != "\r\n" && keyString != "\r") {
+                    input += keyString;
+                    output.getSession().setValue(output.getSession().getValue() + keyString);
+                    scrollDown();
+                } else {
+                    // @TODO: keyCode -> HEX
+                    // input += "\xHEX";
+                    console.log("-> Evento de Keyboard IGNORADO - keyString vazia e suporte a HEX não concluído");
+                }
+            } else {
+                console.log("-> Evento de Keyboard: ", event);
 
-				if (event.type == "keydown" && (event.code.startsWith("Key") || event.code == "Space")) {
-					input += event.key;
-					output.getSession().setValue(output.getSession().getValue() + event.key);
-					scrollDown();
-				}
-			}
-		}
-	});
+                if (event.type == "keydown" && (event.code.startsWith("Key") || event.code == "Space")) {
+                    input += event.key;
+                    output.getSession().setValue(output.getSession().getValue() + event.key);
+                    scrollDown();
+                }
+            }
+        }
+    });
 
-	socket = io(location.protocol + "//" + document.domain + ":" + location.port);
+    socket = io(location.protocol + "//" + document.domain + ":" + location.port);
 
-	socket.on("output", function(data) {
-		console.log("socket.onOutput: " + data);
+    socket.on("output", function(data) {
+        console.log("socket.onOutput: " + data);
 
-		if (input != "" && data.indexOf(input) > -1) {
-			console.log("Ajeitando output...");
-			data = data.substring(input.length + 4); /* ficar de olho nesse 4 aí, pode dar merda em alguma coisa! */
-			input = "";
-			console.log("Output novo: " + data);
-		}
+        if (input != "" && data.indexOf(input) > -1) {
+            console.log("Ajeitando output...");
+            data = data.substring(input.length + 4); /* ficar de olho nesse 4 aí, pode dar merda em alguma coisa! */
+            input = "";
+            console.log("Output novo: " + data);
+        }
 
-		if (data.includes("~|^!+LIMPAR+!^|~")) {
-			data = data.substring(data.indexOf("~|^!+LIMPAR+!^|~") + 16);
-			output.getSession().setValue("");
-		}
+        if (data.includes("~|^!+LIMPAR+!^|~")) {
+            data = data.substring(data.indexOf("~|^!+LIMPAR+!^|~") + 16);
+            output.getSession().setValue("");
+        }
 
-		output.getSession().setValue(output.getSession().getValue() + data);
-		scrollDown();
-	});
+        output.getSession().setValue(output.getSession().getValue() + data);
+        scrollDown();
+    });
 
-	socket.on("hide-response", function(data) {
-		console.log("socket.onHideResponse: " + data);
-		running = false;
-		editor.setReadOnly(false);
-		$("#submit-btn").removeAttr("disabled");
-		$("#stop-btn").attr("disabled", "disabled");
-		scrollDown();
-	});
+    socket.on("hide-response", function(data) {
+        console.log("socket.onHideResponse: " + data);
+        running = false;
+        editor.setReadOnly(false);
+        $("#submit-btn").removeAttr("disabled");
+        $("#stop-btn").attr("disabled", "disabled");
+        scrollDown();
+    });
 
-	socket.on("show-response", function(data) {
-		console.log("socket.onShowResponse: " + data);
-		running = true;
-		editor.setReadOnly(true);
-		$("#submit-btn").attr("disabled", "disabled");
-		$("#stop-btn").removeAttr("disabled");
-		scrollDown();
-	});
+    socket.on("show-response", function(data) {
+        console.log("socket.onShowResponse: " + data);
+        running = true;
+        editor.setReadOnly(true);
+        $("#submit-btn").attr("disabled", "disabled");
+        $("#stop-btn").removeAttr("disabled");
+        scrollDown();
+    });
 
-	$("#save-btn").bind("click", function() {
-		var textEncoder = new TextEncoder("iso-8859-1", { NONSTANDARD_allowLegacyEncoding: true });
-		var contentEncoded = textEncoder.encode([editor.getSession().getValue()]);
+    $("#save-btn").bind("click", function() {
+        var textEncoder = new TextEncoder("iso-8859-1", { NONSTANDARD_allowLegacyEncoding: true });
+        var contentEncoded = textEncoder.encode([editor.getSession().getValue()]);
 
-		var blob = new Blob([contentEncoded], {
-			type: "application/octet-stream; charset=ISO-8859-1",
-		});
+        var blob = new Blob([contentEncoded], {
+            type: "application/octet-stream; charset=ISO-8859-1",
+        });
 
-		if (!efnam) {
-			efnam = "Sem título";
-		}
+        if (!efnam) {
+            efnam = "Sem título";
+        }
 
-		if (efnam.indexOf(".por") == (-1)) {
-			efnam += ".por";
-		}
+        if (efnam.indexOf(".por") == (-1)) {
+            efnam += ".por";
+        }
 
-		saveAs(blob, efnam, false);
-	});
+        saveAs(blob, efnam, false);
+    });
 
-	$("#stop-btn").bind("click", function() {
-		if (!running) return;
+    $("#stop-btn").bind("click", function() {
+        if (!running) return;
 
-		running = false;
-		editor.setReadOnly(false);
-		$("#submit-btn").removeAttr("disabled");
-		$("#stop-btn").attr("disabled", "disabled");
-		socket.disconnect();
-		socket.connect();
-		output.getSession().setValue(output.getSession().getValue() + "\nPrograma finalizado.");
-		scrollDown();
-	});
+        running = false;
+        editor.setReadOnly(false);
+        $("#submit-btn").removeAttr("disabled");
+        $("#stop-btn").attr("disabled", "disabled");
+        socket.disconnect();
+        socket.connect();
+        output.getSession().setValue(output.getSession().getValue() + "\nPrograma finalizado.");
+        scrollDown();
+    });
 
-	$("#submit-btn").bind("click", function() {
-		if (running) return;
+    $("#submit-btn").bind("click", function() {
+        if (running) return;
 
-		running = true;
-		editor.setReadOnly(true);
-		$("#submit-btn").attr("disabled", "disabled");
-		output.getSession().setValue("");
-		scrollDown();
+        running = true;
+        editor.setReadOnly(true);
+        $("#submit-btn").attr("disabled", "disabled");
+        output.getSession().setValue("");
+        scrollDown();
 
-		socket.emit("input", editor.getSession().getValue() + "\r");
-	});
+        socket.emit("input", editor.getSession().getValue() + "\r");
+    });
 
-	$("#layout").ax5layout({
-		onResize: function() {
-			editor.resize();
-			output.resize();
-		}
-	});
+    $("#layout").ax5layout({
+        onResize: function() {
+            editor.resize();
+            output.resize();
+        }
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -40,16 +40,16 @@ router.group("/ide", function(router) {
             if(extName == ".htm" || extName == ".html"){
                 data = data.toString('utf8');
                 var style = ".dp-highlighter{pointer-events:initial !important}html,body{margin:0;padding:0;font-family:'PT Sans',sans-serif;font-size:15px}body{padding-bottom:25px}p{line-height:18pt}h1,h2{padding-left:15px;font-family:'PT Sans',sans-serif}h1{font-size:18pt}ul>li{margin-top:10px}ul>li:first-child{margin-top:0}";
-              	data = data.replace("<head>", "<head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script type=\"text/javascript\">var d={baseURL:\""+ baseRoute +"\"};</script>");
-              	data = data.replace("</head>", "<link href=\"https://fonts.googleapis.com/css?family=PT+Sans\" rel=\"stylesheet\"><style type=\"text/css\">" + style + "</style></head>");
-              	data = data.replaceArray(["../../../../scripts/exemplos.js", "../../../scripts/exemplos.js", "../../scripts/exemplos.js"], baseURL + "assets/editor/Recursos-exemplos.js");
+                  data = data.replace("<head>", "<head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><script type=\"text/javascript\">var d={baseURL:\""+ baseRoute +"\"};</script>");
+                  data = data.replace("</head>", "<link href=\"https://fonts.googleapis.com/css?family=PT+Sans\" rel=\"stylesheet\"><style type=\"text/css\">" + style + "</style></head>");
+                  data = data.replaceArray(["../../../../scripts/exemplos.js", "../../../scripts/exemplos.js", "../../scripts/exemplos.js"], baseURL + "assets/editor/Recursos-exemplos.js");
                 data = data.replaceArray(["../../../../scripts/", "../../../scripts/", "../../scripts/"], baseURL + "Recursos/ajuda/scripts/");
-              	data = data.replaceArray(["../../../../estilos/", "../../../estilos/", "../../estilos/"], baseURL + "Recursos/ajuda/estilos/");
-              	data = data.replaceArray(["../../../../recursos/imagens/", "../../../recursos/imagens/", "../../recursos/imagens/"], baseURL + "Recursos/ajuda/recursos/imagens/");
-              	data = data.replaceArray(["../../../../recursos/", "../../../recursos/", "../../recursos/"], "Recursos/ajuda/recursos/");
-              	data = data.replace(new RegExp("topicos/linguagem_portugol/", "g"), baseRoute + "resp?file=Recursos/ajuda/topicos/linguagem_portugol/");
-              	data = data.replace("${ajuda}", "Dark/ajuda.css");
-              	data = data.replace("${syntax}", "Dark/SyntaxHighlighter.css");
+                  data = data.replaceArray(["../../../../estilos/", "../../../estilos/", "../../estilos/"], baseURL + "Recursos/ajuda/estilos/");
+                  data = data.replaceArray(["../../../../recursos/imagens/", "../../../recursos/imagens/", "../../recursos/imagens/"], baseURL + "Recursos/ajuda/recursos/imagens/");
+                  data = data.replaceArray(["../../../../recursos/", "../../../recursos/", "../../recursos/"], "Recursos/ajuda/recursos/");
+                  data = data.replace(new RegExp("topicos/linguagem_portugol/", "g"), baseRoute + "resp?file=Recursos/ajuda/topicos/linguagem_portugol/");
+                  data = data.replace("${ajuda}", "Dark/ajuda.css");
+                  data = data.replace("${syntax}", "Dark/SyntaxHighlighter.css");
             } else if (extName == ".por") {
               data = iconv.decode(data, "ISO-8859-1");
               if (data.indexOf("/* $$$ Portugol Studio $$$ ") > -1) {
