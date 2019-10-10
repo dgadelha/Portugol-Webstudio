836041b5fe37efec06594dc511dbe9af957d4c1b
Douglas Gadêlha <dgadelha@live.com>
2019-10-10 02:05:06 -0300

    Atualizar o .NET Core para a versão 2.2

--- runtime/compile_linux.sh
+++ runtime/compile_linux.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-dotnet restore
-dotnet publish -r debian.8-x64
-rm -rf ./bin/Debug/netcoreapp2.0/debian.8-x64/publish
+#!/bin/sh
+if [ -e "/lib/libc.musl-x86_64.so.1" ]; then
+    RUNTIME_ID="linux-musl-x64"
+else
+    RUNTIME_ID="linux-x64"
+fi
+
+dotnet publish -c Release -r $RUNTIME_ID
 rm -rf dist
-mkdir dist
-mv ./bin/Debug/netcoreapp2.0/debian.8-x64/ ./dist/debian.8-x64/
-mkdir ./dist/debian.8-x64/temp/
-rm -rf ./bin/
+mv ./bin/Release/netcoreapp2.2/$RUNTIME_ID/publish ./dist
+mkdir ./dist/temp/
