3cd424e61ff5499aedd4f71d5ac4d8b30e7f365c
Douglas Gadêlha <dgadelha@live.com>
2019-10-28 11:25:04 -0300

    Corrigir expressão incorreta e atualizar pacotes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname , 'views'));
 app.set('view engine', 'twig');
 
 app.use(logger('dev', {
-    skip: req => req.url !== "_health"
+    skip: req => req.url === "/_health"
 }));
 
 app.use(express.static(path.join(__dirname , 'public')));
