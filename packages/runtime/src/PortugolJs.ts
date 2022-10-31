3fede2a1e9eb1a5b02af192ca0b2c370421e2483
Danilo Gadelha <dngadelha@outlook.com>
2022-10-31 19:47:51 -0300

    Implementação dos operadores de atribuição composta (+=, -=, /= e *=)

--- packages/runtime/src/PortugolJs.ts
+++ packages/runtime/src/PortugolJs.ts
@@ -794,70 +794,150 @@ export class PortugolJs extends AbstractParseTreeVisitor<string> implements Port
     return sb.toString();
   }
 
-  // TODO
   visitAtribuicaoCompostaSoma(ctx: AtribuicaoCompostaSomaContext) {
     const sb = new StringBuilder();
 
-    if (!PortugolJs.thrown.visitAtribuicaoCompostaSoma) {
-      captureException("visitAtribuicaoCompostaSoma", {
-        extra: { text: ctx.text },
-      });
-      PortugolJs.thrown.visitAtribuicaoCompostaSoma = true;
+    sb.append(this.DEBUG(`visitAtribuicaoCompostaSoma`, ctx));
+    sb.append(this.PAD(), `runtime.assign([`, `\n`);
+
+    this.pad++;
+
+    const exprs = ctx.expressao();
+    const first = exprs.shift();
+
+    if (first) {
+      sb.append(super.visit(first).trimEnd());
+      sb.append(`,\n`);
+
+      for (const expr of exprs) {
+        sb.append(this.PAD(), `runtime.mathOperation("+", [`, `\n`);
+
+        this.pad++;
+
+        sb.append(super.visit(first).trimEnd(), ",", `\n`);
+
+        sb.append(super.visit(expr).trimEnd(), ",\n");
+
+        this.pad--;
+
+        sb.append(this.PAD(), "])", ",", `\n`);
+      }
     }
 
-    sb.append(this.DEBUG(`visitAtribuicaoCompostaSoma`, ctx));
-    sb.append(super.visitChildren(ctx));
+    this.pad--;
+
+    sb.append(this.PAD(), `])`, `\n`);
 
     return sb.toString();
   }
 
-  // TODO
   visitAtribuicaoCompostaSubtracao(ctx: AtribuicaoCompostaSubtracaoContext) {
     const sb = new StringBuilder();
 
-    if (!PortugolJs.thrown.visitAtribuicaoCompostaSubtracao) {
-      captureException("visitAtribuicaoCompostaSubtracao", {
-        extra: { text: ctx.text },
-      });
-      PortugolJs.thrown.visitAtribuicaoCompostaSubtracao = true;
+    sb.append(this.DEBUG(`visitAtribuicaoCompostaSubtracao`, ctx));
+    sb.append(this.PAD(), `runtime.assign([`, `\n`);
+
+    this.pad++;
+
+    const exprs = ctx.expressao();
+    const first = exprs.shift();
+
+    if (first) {
+      sb.append(super.visit(first).trimEnd());
+      sb.append(`,\n`);
+
+      for (const expr of exprs) {
+        sb.append(this.PAD(), `runtime.mathOperation("-", [`, `\n`);
+
+        this.pad++;
+
+        sb.append(super.visit(first).trimEnd(), ",", `\n`);
+
+        sb.append(super.visit(expr).trimEnd(), ",\n");
+
+        this.pad--;
+
+        sb.append(this.PAD(), "])", ",", `\n`);
+      }
     }
 
-    sb.append(this.DEBUG(`visitAtribuicaoCompostaSubtracao`, ctx));
-    sb.append(super.visitChildren(ctx));
+    this.pad--;
+
+    sb.append(this.PAD(), `])`, `\n`);
 
     return sb.toString();
   }
 
-  // TODO
   visitAtribuicaoCompostaMultiplicacao(ctx: AtribuicaoCompostaMultiplicacaoContext) {
     const sb = new StringBuilder();
 
-    if (!PortugolJs.thrown.visitAtribuicaoCompostaMultiplicacao) {
-      captureException("visitAtribuicaoCompostaMultiplicacao", {
-        extra: { text: ctx.text },
-      });
-      PortugolJs.thrown.visitAtribuicaoCompostaMultiplicacao = true;
+    sb.append(this.DEBUG(`visitAtribuicaoCompostaMultiplicacao`, ctx));
+    sb.append(this.PAD(), `runtime.assign([`, `\n`);
+
+    this.pad++;
+
+    const exprs = ctx.expressao();
+    const first = exprs.shift();
+
+    if (first) {
+      sb.append(super.visit(first).trimEnd());
+      sb.append(`,\n`);
+
+      for (const expr of exprs) {
+        sb.append(this.PAD(), `runtime.mathOperation("*", [`, `\n`);
+
+        this.pad++;
+
+        sb.append(super.visit(first).trimEnd(), ",", `\n`);
+
+        sb.append(super.visit(expr).trimEnd(), ",\n");
+
+        this.pad--;
+
+        sb.append(this.PAD(), "])", ",", `\n`);
+      }
     }
 
-    sb.append(this.DEBUG(`visitAtribuicaoCompostaMultiplicacao`, ctx));
-    sb.append(super.visitChildren(ctx));
+    this.pad--;
+
+    sb.append(this.PAD(), `])`, `\n`);
 
     return sb.toString();
   }
 
-  // TODO
   visitAtribuicaoCompostaDivisao(ctx: AtribuicaoCompostaDivisaoContext) {
     const sb = new StringBuilder();
 
-    if (!PortugolJs.thrown.visitAtribuicaoCompostaDivisao) {
-      captureException("visitAtribuicaoCompostaDivisao", {
-        extra: { text: ctx.text },
-      });
-      PortugolJs.thrown.visitAtribuicaoCompostaDivisao = true;
+    sb.append(this.DEBUG(`visitAtribuicaoCompostaDivisao`, ctx));
+    sb.append(this.PAD(), `runtime.assign([`, `\n`);
+
+    this.pad++;
+
+    const exprs = ctx.expressao();
+    const first = exprs.shift();
+
+    if (first) {
+      sb.append(super.visit(first).trimEnd());
+      sb.append(`,\n`);
+
+      for (const expr of exprs) {
+        sb.append(this.PAD(), `runtime.mathOperation("/", [`, `\n`);
+
+        this.pad++;
+
+        sb.append(super.visit(first).trimEnd(), ",", `\n`);
+
+        sb.append(super.visit(expr).trimEnd(), ",\n");
+
+        this.pad--;
+
+        sb.append(this.PAD(), "])", ",", `\n`);
+      }
     }
 
-    sb.append(this.DEBUG(`visitAtribuicaoCompostaDivisao`, ctx));
-    sb.append(super.visitChildren(ctx));
+    this.pad--;
+
+    sb.append(this.PAD(), `])`, `\n`);
 
     return sb.toString();
   }
