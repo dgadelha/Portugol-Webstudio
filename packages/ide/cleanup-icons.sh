e432398d644909112ee65d5c375a7a37472920f7
Douglas Gadêlha <dgadelha@live.com>
2024-08-05 22:53:54 -0300

    Realizar limpeza de ícones não utilizados

---
+++ packages/ide/cleanup-icons.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+## Remove os ícones que não são usados no projeto, para diminuir o download realizado pelo Service Worker
+
+pushd "$(dirname ${BASH_SOURCE:0})" || exit 1
+trap popd EXIT
+shopt -s globstar nullglob
+
+BASE_DIR="dist/browser/assets/mdi"
+
+if [ ! -d "$BASE_DIR" ]; then
+  echo "Error: $BASE_DIR does not exist"
+  exit 1
+fi
+
+for i in "$BASE_DIR"/*.svg; do
+  ICON_NAME=$(basename "$i")
+
+  # Check if any file inside src/ uses the icon
+  if grep -q "$ICON_NAME" src/**/*.ts src/**/*.html src/**/*.scss; then
+    echo "skipped $ICON_NAME"
+    continue
+  fi
+
+  rm "$i"
+done
