9a9774a68c483cb629d2935bddce8ca1177c0746
dgadelha <dgadelha@users.noreply.github.com>
2025-02-19 00:04:08 +0000

    Deploying to gh-pages from @ dgadelha/Portugol-Webstudio@3239a849c7aafa1f260fa017d2619f49067c5121 🚀

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1069,7 +1069,7 @@ ${error.stack}`;
   };
 
   // bazel-out/darwin_arm64-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "19.1.5";
+  var SW_VERSION = "19.1.6";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
