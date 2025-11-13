8cb67b96c35e40bd5ba3c8bd822242049c41df97
dgadelha <dgadelha@users.noreply.github.com>
2025-11-13 21:21:25 +0000

    Deploying to gh-pages from @ dgadelha/Portugol-Webstudio@33b7b8258d328fe7fd61b8aa56305f3a4df75494 🚀

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "20.3.10";
+  var SW_VERSION = "20.3.11";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
@@ -2005,7 +2005,6 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ "Content-Type": "text/plain" }
         return;
       }
       const brokenHash = broken[0];
-      await this.notifyClientsAboutVersionFailure(brokenHash, err);
       if (this.latestHash === brokenHash) {
         this.state = DriverReadyState.EXISTING_CLIENTS_ONLY;
         this.stateMessage = `Degraded due to: ${errorToString(err)}`;
@@ -2195,21 +2194,6 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ "Content-Type": "text/plain" }
           latestVersion: this.mergeHashWithAppData(manifest, hash)
         };
         client.postMessage(notice);
-      }));
-    }
-    async notifyClientsAboutVersionFailure(brokenHash, error) {
-      await this.initialized;
-      const affectedClients = Array.from(this.clientVersionMap.entries()).filter(([clientId, hash]) => hash === brokenHash).map(([clientId]) => clientId);
-      await Promise.all(affectedClients.map(async (clientId) => {
-        const client = await this.scope.clients.get(clientId);
-        if (client) {
-          const brokenVersion = this.versions.get(brokenHash);
-          client.postMessage({
-            type: "VERSION_FAILED",
-            version: this.mergeHashWithAppData(brokenVersion.manifest, brokenHash),
-            error: errorToString(error)
-          });
-        }
       }));
     }
     async broadcast(msg) {
