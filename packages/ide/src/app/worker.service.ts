26bc6513c7e676e081edf94ad82dc9125a52be7f
Douglas Gadêlha <dgadelha@live.com>
2026-04-10 19:20:44 -0300

    Teste: Evitar cache no worker

--- packages/ide/src/app/worker.service.ts
+++ packages/ide/src/app/worker.service.ts
@@ -7,7 +7,7 @@ export class WorkerService {
   busy = false;
 
   init() {
-    this.worker = new Worker("assets/portugol-worker/worker.js");
+    this.worker = new Worker("assets/portugol-worker/worker.js?v=" + Date.now().toString());
   }
 
   async checkCode(code: string): Promise<{
